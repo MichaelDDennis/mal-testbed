556d58abae508ce9947dedf77cbe0abf528ccf37
Drainbot <jamesdrain95@gmail.com>
2018-06-25 15:04:13 -0700

    fixed minimize bug/ changing others strats

--- gdtest.py
+++ gdtest.py
@@ -2,22 +2,23 @@ import tensorflow as tf
 import numpy as np
 import time
 
-w = tf.Variable([0.5, 0.5], name="w")
+a = tf.Variable([0.5], name="a")
+b = tf.Variable([0.5], name="b")
 
 cc=400
 cd=100
 dc=450
 dd=350
 
-probca=tf.multiply(tf.tanh(w[0]),0.5)+0.5
-probcb=tf.multiply(tf.tanh(w[1]),0.5)+0.5
+probca=tf.multiply(tf.tanh(a[0]),0.5)+0.5
+probcb=tf.multiply(tf.tanh(b[0]),0.5)+0.5
 
 ua = tf.multiply(tf.multiply(probca,  probcb),cc)+tf.multiply(tf.multiply(probca, 1- probcb),cd)+tf.multiply(tf.multiply(1-probca,  probcb),dc)+tf.multiply(tf.multiply(1-probca, 1- probcb),dd)
 ub = tf.multiply(tf.multiply(probca,  probcb),cc)+tf.multiply(tf.multiply(probca, 1- probcb),dc)+tf.multiply(tf.multiply(1-probca,  probcb),cd)+tf.multiply(tf.multiply(1-probca, 1- probcb),dd)
 
 # The Gradient Descent Optimizer does the heavy lifting
-dua = tf.train.GradientDescentOptimizer(0.01).minimize(ua)
-dub = tf.train.GradientDescentOptimizer(0.01).minimize(ub)
+dua = tf.train.GradientDescentOptimizer(0.01).minimize(0-ua, var_list=[a])
+dub = tf.train.GradientDescentOptimizer(0.01).minimize(0-ub, var_list=[b])
 
 # Normal TensorFlow - initialize values, create a session and run the model
 model = tf.global_variables_initializer()

dee159b4d6964fc0f0e42c637257ba9a481fafe5
Michael Dennis <michael_dennis@cs.berkeley.edu>
2018-06-26 17:19:11 -0700

    cleaned code

--- gdtest.py
+++ gdtest.py
@@ -36,17 +36,41 @@ ub=ub1+ub2
 dua = tf.train.GradientDescentOptimizer(0.01).minimize(0-ua, var_list=[a])
 dub = tf.train.GradientDescentOptimizer(0.01).minimize(0-ub, var_list=[b])
 
+#The Default Print Function
+def printF(s):
+    print(s)
 
-
-
-
-
-def Simulate(InitialState,InitialMA,InitialMB,printState,Dyn,ObsA,ObsB,DTA,DTB):
+# Simulate is the Main function we will be studying, the structure here is meant to force isolation between 
+# different parts of the code base so it is easier to tell what is going on and who knows about who.  Ideally
+# there would be no global state, and all model parameters, would be fed through ma/mb.
+#
+# One specific way in which this helps avoid confusion is in the seperating the game itself from how they 
+# update their models.  Though both will have similar structure, one will be in the agents head and the 
+# other will be in the real world.  Only the first needs to be differentiable and they need not always match.
+#
+# For clarity, the types of all of the arguments are as follows:
+#
+# Initial State: State
+# Dyn: State x ActA x ActB --> State
+# ObsA: State -> ObsA
+# ObsB: State -> ObsB
+#
+# A: (InitialMA,DTA)
+#   InitialMA: HiddenStateA
+#   DTA: ObsA x HiddenStateA --> ActA x HiddenStateA
+#
+# B: (InitialMB,DTB)
+#   InitialMB: HiddenStateB
+#   DTB: ObsB x HiddenStateB --> ActB x HiddenStateB
+#
+def Simulate(InitialState,Dyn,ObsA,ObsB,A,B,printState=printF):
     # Normal TensorFlow - initialize values, create a session and run the model
      
     state=InitialState
-    ma=InitialMA
-    mb=InitialMB
+    ma=A[0]
+    mb=B[0]
+    DTA=A[1]
+    DTB=B[1]
 
     for i in range(1000):
         a,ma=DTA(ObsA(state),ma)
@@ -57,24 +81,28 @@ def Simulate(InitialState,InitialMA,InitialMB,printState,Dyn,ObsA,ObsB,DTA,DTB):
         printState(state)
         time.sleep(1)
         
-    
 
+
+#State Dynamics which do not depend on the last state, and give a unique state for every action pair
 def ActionPairDyn(s,a,b):
     return {'lasta':a,'lastb':b}
 
+#Observation Function for Fully Observable Enviroments
 def TransparentObs(s):
     return s
 
+#Some Dummy Decsion Rules for testing purposes
 def TitForTatA(o,m):
     return (o['lastb'],m)
 
 def TitForTatB(o,m):
     return (o['lasta'],m)
 
-def printF(s):
-    print(s)
 
 
+
+
+#The Next Two decision theories update their models via gradient decent and use that to give the next action 
 def GradDecentDTA(o,m):
     #Currently Storing the policy in global space instead of in m
     #Storing in m would be nicer since it makes the dependencies clear
@@ -99,11 +127,14 @@ def GradDecentDTB(o,m):
 
 
 
+
 #Simulate({'lasta':1,'lastb':0},0,0,printF,ActionPairDyn,TransparentObs,TransparentObs,TitForTatA,TitForTatB)
+
+#Setting up tensor flow before running the simulation
 model = tf.global_variables_initializer()
 with tf.Session() as session:
     session.run(model)
-    Simulate({'lasta':0.2,'lastb':0.5},0,0,printF,ActionPairDyn,TransparentObs,TransparentObs,GradDecentDTA,GradDecentDTB)
+    Simulate({'lasta':0.2,'lastb':0.5},ActionPairDyn,TransparentObs,TransparentObs,(0,GradDecentDTA),(0,GradDecentDTB))
 
 
 

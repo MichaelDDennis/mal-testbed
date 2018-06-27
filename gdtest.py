ebb4ca2e03ec4c23b9a69536e42cc6b1393ea51c
Michael Dennis <michael_dennis@cs.berkeley.edu>
2018-06-26 21:56:38 -0700

    Did a bit more refactoring.  Next step would be to seperate out each step of the game in each player's heads and making explict variables for modeling their opponents

--- gdtest.py
+++ gdtest.py
@@ -2,60 +2,6 @@ import tensorflow as tf
 import numpy as np
 import time
 
-last_a =tf.placeholder(tf.float32)
-last_b =tf.placeholder(tf.float32)
-
-
-opp    =tf.placeholder(tf.float32,(3,))
-
-
-cc=400
-cd=0
-dc=401
-dd=50
-
-
-
-def getUtilFun(cc,cd,dc,dd,probca,probcb):
-    u = tf.multiply(tf.multiply(probca,  probcb),cc)+tf.multiply(tf.multiply(probca, 1- probcb),cd)+tf.multiply(tf.multiply(1-probca,  probcb),dc)+tf.multiply(tf.multiply(1-probca, 1- probcb),dd)
-    return u
-
-
-
-#All of the variables and Update for A
-a = tf.Variable([0.0,100000.0,0.0], name="a")
-
-probca     =tf.multiply(tf.tanh(tf.multiply(  a[0],last_a)+tf.multiply(  a[1],last_b)+  a[2]),0.5)+0.5
-probcb_opp =tf.multiply(tf.tanh(tf.multiply(opp[0],last_a)+tf.multiply(opp[1],last_b)+opp[2]),0.5)+0.5
-
-probca2    =tf.multiply(tf.tanh(tf.multiply(  a[0],probca)+tf.multiply(  a[1],probcb_opp)+  a[2]),0.5)+0.5
-probcb2_opp=tf.multiply(tf.tanh(tf.multiply(opp[0],probca)+tf.multiply(opp[1],probcb_opp)+opp[2]),0.5)+0.5
-
-
-
-
-ua1= getUtilFun(cc,cd,dc,dd,probca,probcb_opp)
-ua2= getUtilFun(cc,cd,dc,dd,probca2,probcb2_opp)
-ua=ua1+ua2
-dua = tf.train.GradientDescentOptimizer(0.01).minimize(0-ua, var_list=[a])
-
-
-#All of the Variables and Update for B
-b = tf.Variable([100000.0,0.0,0.0], name="b")
-
-probca_opp  =tf.multiply(tf.tanh(tf.multiply(opp[0],last_a)+tf.multiply(opp[1],last_b)+opp[2]),0.5)+0.5
-probcb      =tf.multiply(tf.tanh(tf.multiply(  b[0],last_a)+tf.multiply(  b[1],last_b)+  b[2]),0.5)+0.5
-
-probca2_opp =tf.multiply(tf.tanh(tf.multiply(opp[0],probca_opp)+tf.multiply(opp[1],probcb)+opp[2]),0.5)+0.
-probcb2     =tf.multiply(tf.tanh(tf.multiply(  b[0],probca_opp)+tf.multiply(  b[1],probcb)+  b[2]),0.5)+0.5
-
-
-
-ub1= getUtilFun(cc,cd,dc,dd,probcb,probca_opp)
-ub2= getUtilFun(cc,cd,dc,dd,probcb2,probca2_opp)
-ub=ub1+ub2
-dub = tf.train.GradientDescentOptimizer(0.01).minimize(0-ub, var_list=[b])
-
 
 #The Default Print Function
 def printF(s):
@@ -146,6 +92,67 @@ def Transparency(a):
         a['opp']=session.run(mb['me'])
     return temp
 
+last_a =tf.placeholder(tf.float32)
+last_b =tf.placeholder(tf.float32)
+opp    =tf.placeholder(tf.float32,(3,))
+
+
+payoff=[[400,0],
+        [401,50]]
+
+
+
+def getMixedUtilFun(mat,pa,pb):
+    u=0
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            u=u+tf.multiply(tf.multiply(pa[i],pb[j]),mat[i][j])
+    return u
+
+
+
+
+
+#All of the variables and Update for A
+a = tf.Variable([0.0,100000.0,0.0], name="a")
+
+probca     =tf.multiply(tf.tanh(tf.multiply(  a[0],last_a)+tf.multiply(  a[1],last_b)+  a[2]),0.5)+0.5
+probcb_opp =tf.multiply(tf.tanh(tf.multiply(opp[0],last_a)+tf.multiply(opp[1],last_b)+opp[2]),0.5)+0.5
+
+probca2    =tf.multiply(tf.tanh(tf.multiply(  a[0],probca)+tf.multiply(  a[1],probcb_opp)+  a[2]),0.5)+0.5
+probcb2_opp=tf.multiply(tf.tanh(tf.multiply(opp[0],probca)+tf.multiply(opp[1],probcb_opp)+opp[2]),0.5)+0.5
+
+probcaPA     =[probca     ,1-probca]
+probcb_oppPA =[probcb_opp ,1-probcb_opp]
+probca2PA    =[probca2    ,1-probca2]
+probcb2_oppPA=[probcb2_opp,1-probcb2_opp]
+
+
+ua1= getMixedUtilFun(payoff,probcaPA,probcb_oppPA)
+ua2= getMixedUtilFun(payoff,probca2PA,probcb2_oppPA)
+ua=ua1+ua2
+dua = tf.train.GradientDescentOptimizer(0.01).minimize(0-ua, var_list=[a])
+
+
+#All of the Variables and Update for B
+b = tf.Variable([100000.0,0.0,0.0], name="b")
+
+probca_opp  =tf.multiply(tf.tanh(tf.multiply(opp[0],last_a)+tf.multiply(opp[1],last_b)+opp[2]),0.5)+0.5
+probcb      =tf.multiply(tf.tanh(tf.multiply(  b[0],last_a)+tf.multiply(  b[1],last_b)+  b[2]),0.5)+0.5
+
+probca2_opp =tf.multiply(tf.tanh(tf.multiply(opp[0],probca_opp)+tf.multiply(opp[1],probcb)+opp[2]),0.5)+0.5
+probcb2     =tf.multiply(tf.tanh(tf.multiply(  b[0],probca_opp)+tf.multiply(  b[1],probcb)+  b[2]),0.5)+0.5
+
+probca_oppPA     =[probca_opp  ,1-probca_opp]
+probcbPA         =[probcb      ,1-probcb]
+probca2_oppPA    =[probca2_opp ,1-probca2_opp]
+probcb2PA        =[probcb2     ,1-probcb2]
+
+
+ub1= getMixedUtilFun(payoff,probcbPA,probca_oppPA)
+ub2= getMixedUtilFun(payoff,probcb2PA,probca2_oppPA)
+ub=ub1+ub2
+dub = tf.train.GradientDescentOptimizer(0.01).minimize(0-ub, var_list=[b])
 
 
 #Setting up tensor flow before running the simulation

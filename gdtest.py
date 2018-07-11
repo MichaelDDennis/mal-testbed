ceec65b54739ece09217d222de50c85b1e960fb1
Michael Dennis <michael_dennis@cs.berkeley.edu>
2018-07-10 17:45:27 -0700

    Added Sampling to the simulation and finite-depth prediction to the gradient decent

--- gdtest.py
+++ gdtest.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from numpy import random
 from abc import ABC, abstractmethod
 import time
 
@@ -49,7 +50,7 @@ class GradientDecentBasedAgent(ModelBasedAgent):
         session.run(self._update, feed_dict=self._get_state(observation))
 
     def predict(self, observation):
-        action = session.run(self._predict, feed_dict=self._get_state(observation)).item()
+        action = session.run(self._predict, feed_dict=self._get_state(observation))
         return action
 
     def _get_state(self, observation):
@@ -66,6 +67,20 @@ class TransparentAgentDecorator(Agent):
 
     def get_action(self, observation):
         return {'action': self._agent.get_action(observation), 'model': self._get_model()}
+
+
+def sample(distr):
+    return (random.choice(range(len(distr)), 1, distr)[0]*1.0).item()
+
+
+class SamplingAgentDecorator(Agent):
+    def __init__(self, agent):
+        super().__init__()
+        self._agent = agent
+
+    def get_action(self, observation):
+        action = self._agent.get_action(observation)
+        return sample(action)
 
 
 # Simulate is the Main function we will be studying, the structure here is meant to force isolation between 
@@ -155,10 +170,10 @@ def get_mixed_utility_function(mat):
 
         a = state['me_action_node']
         b = state['opp_action_node']
-        pa = [a, 1 - a]
-        pb = [b, 1 - b]
+        pa = [a, 1.0 - a]
+        pb = [b, 1.0 - b]
 
-        u = 0
+        u = 0.0
         for i in range(len(mat)):
             for j in range(len(mat)):
                 u = u+tf.multiply(tf.multiply(pa[i], pb[j]), mat[i][j])
@@ -166,12 +181,11 @@ def get_mixed_utility_function(mat):
     return utility_function
 
 
-def get_discounted_utility(reward, state_list, discount=1):
-    res = 0
-    current_value = 1
-    for state in state_list:
-        res += reward(state) * current_value
-        current_value *= discount
+def get_discounted_utility(reward, state_list):
+    res = 0.0
+    for state, prob in state_list:
+        r = tf.multiply(reward(state), prob)
+        res += r
     return res
 
 
@@ -181,21 +195,24 @@ def make_agent(start_vector):
     opp = tf.placeholder(tf.float32, (3,))
     me = tf.Variable(start_vector, name="me")
 
-    payoff = [[400, 0],
-              [401, 50]]
+    payoff = [[400.0, 0.0],
+              [401.0, 50.0]]
 
     # TODO add easy way to bound probabilities within the computation graphs
     # TODO add an easy way to bound probabilities during updates
     # TODO add an easy way to build "complete" policy spaces
+
     def me_model(observation, me_vars):
-        return tf.multiply(tf.tanh(tf.multiply(me_vars[0], observation['me_action_node']) +
-                                   tf.multiply(me_vars[1], observation['opp_action_node']) + me_vars[2]), 0.5) + 0.5
+        prob_c = tf.multiply(tf.tanh(tf.multiply(me_vars[0], observation['me_action_node']) +
+                                     tf.multiply(me_vars[1], observation['opp_action_node']) + me_vars[2]), 0.5) + 0.5
+        return [prob_c, 1.0 - prob_c]
 
     def opp_model(observation, opp_vars):
-        return tf.multiply(tf.tanh(tf.multiply(opp_vars[0], observation['opp_action_node']) +
-                                   tf.multiply(opp_vars[1], observation['me_action_node']) + opp_vars[2]), 0.5) + 0.5
+        prob_c = tf.multiply(tf.tanh(tf.multiply(opp_vars[0], observation['opp_action_node']) +
+                                     tf.multiply(opp_vars[1], observation['me_action_node']) + opp_vars[2]), 0.5) + 0.5
+        return [prob_c, 1.0 - prob_c]
 
-    # TODO Fix Dynamics Model so that it is sampling-based instead of distribution game.
+    # We can always add a random player, dynamics can be deterministic
     def dyn_model(_, me_action, them_action):
         return {'me_action_node': me_action, 'opp_action_node': them_action}
 
@@ -212,17 +229,27 @@ def make_agent(start_vector):
     def opp_update_model(opp, _):
         return opp
 
-    opp_cur = opp
-    me_cur = me
     initial_state = {'me_action_node': last_me, 'opp_action_node': last_opp}
-    state = initial_state
-    states=[]
-    for i in range(1):
-        state = dyn_model(state, me_model(me_observation_model(state), me_cur),
-                          opp_model(opp_observation_model(state), opp_cur))
-        me_cur = me_update_model(me, me_observation_model(state))
-        opp_cur = opp_update_model(opp, opp_observation_model(state))
-        states.append(state)
+    states = []
+    full_states_to_process = [{'state': initial_state, 'me_model': me, 'opp_model': opp, 'depth': 0}]
+    for full_state in full_states_to_process:
+        state = full_state['state']
+        me_cur = full_state['me_model']
+        opp_cur = full_state['opp_model']
+        action_distr_me = me_model(me_observation_model(state), me_cur)
+        action_distr_opp = opp_model(opp_observation_model(state), opp_cur)
+
+        for action_me in range(1):
+            for action_opp in range(1):
+                new_state = dyn_model(state, 1.0*action_me, 1.0*action_opp)
+                prob = tf.multiply(action_distr_me[action_me], action_distr_opp[action_opp])
+                states.append((new_state, prob))
+
+                if full_state['depth'] < 2:
+                    full_states_to_process.append({'state': new_state,
+                                                   'me_model': me_update_model(me_cur, me_observation_model(state)),
+                                                   'opp_model': opp_update_model(opp_cur, opp_observation_model(state)),
+                                                   'depth': full_state['depth']+1})
 
     u = get_discounted_utility(get_mixed_utility_function(payoff), states)
 
@@ -235,8 +262,8 @@ def make_agent(start_vector):
     def get_model():
         return session.run(me)
 
-    return TransparentAgentDecorator(GradientDecentBasedAgent(me_model(me_observation_model(initial_state), me),
-                                                              u, me, make_state), get_model)
+    return TransparentAgentDecorator(SamplingAgentDecorator(GradientDecentBasedAgent(
+        me_model(me_observation_model(initial_state), me), u, me, make_state)), get_model)
 
 
 def main():
